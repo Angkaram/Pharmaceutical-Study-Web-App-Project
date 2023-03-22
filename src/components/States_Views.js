63acd398189797220ebaa466b1e9d55227d6394b
rahulio96 <rahgupta96@gmail.com>
2023-03-21 20:58:42 -0700

    Logging in directly sends you to your respective view, also added bavaria view

--- src/components/States_Views.js
+++ src/components/States_Views.js
@@ -8,58 +8,57 @@ import ValidateDomain from "./validation";
 import AddPatientButton from './addButton.js';
 import DisplayPatientData from './DisplayPatientData';
 
+let view;
+
 function View() {
   
   // the email for the user is displayed.
   // changes based on state, role, and view
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+
+  let navigate = useNavigate();
+  
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
-      const user = {
+      const userInfo = {
         email: userAuth?.email,
         role: userAuth?.displayName,
         id: userAuth?.uid
       }
       if (userAuth) {
         console.log(userAuth)
-        setUser(user)
+        setUser(userInfo)
       } else {
         setUser(null)
       }
-    })
-    return unsubscribe
-  }, []);
 
-  // eslint-disable-next-line
-  const [activeButton, setActiveButton] = useState(null); // buttons to change states
-  const [currentView, setCurrentView] = useState('Welcome'); // so that we can go "back" to different views
+      const logout = async () => {
+        await signOut(auth);
+        navigate("/");
+      };
 
-  const handleButtonClick = (buttonName) => { // when we click on a button, it sets it to activeButton and
-    let roleString = buttonName.toLowerCase();
-    if (user.role === roleString && ValidateDomain(user.email, roleString) === true) {
-      setActiveButton(buttonName);	      // bases the view based on the active button clicked
-      setCurrentView(buttonName + 'View');
+      // Validates the user
+      let isValidated = ValidateDomain(userInfo.email, userInfo.role);
+
+      // Checks their role and redirects them accordingly
+      if (isValidated === true) {
+        if (userInfo.role === 'doctor') {
+          view = <DoctorView user = {user} LogOut = {logout} />;
+        } else if (userInfo.role === "fda") {
+          view = <FDAView user = {user} LogOut = {logout}/>;
+        } else if (userInfo.role === "bavaria") {
+          view = <BavariaView user = {user} LogOut = {logout}/>;
+        }
+    // If everything fails, kicks unauthorized user to the login page
     } else {
-      alert("You do not have access to this view");
+      navigate("/Login");
     }
-  };
 
-  const handleBackButtonClick = () => {
-    setActiveButton(null); // null takes us back to default state of Home view, so pressing back takes us back
-    setCurrentView('Welcome'); // view corresponds to back button click
-  };
+    })
 
-  let view;
-  // all the different options and views based on button clicks
-  if (currentView === 'Welcome') {
-    view = <WelcomeView handleButtonClick={handleButtonClick} />;
-  } else if (currentView === 'PatientView') {
-    view = <PatientView user = {user} handleBackButtonClick={handleBackButtonClick} />;
-  } else if (currentView === 'DoctorView') {
-    view = <DoctorView user = {user} handleBackButtonClick={handleBackButtonClick} />;
-  } else if (currentView === 'FDAView') {
-    view = <FDAView user = {user} handleBackButtonClick={handleBackButtonClick} />;
-  }
+    return unsubscribe
+// eslint-disable-next-line
+  }, []);
 
   // styling
   return (
@@ -67,50 +66,10 @@ function View() {
       {view}
     </div>
   );
-}
-
-// what is shown on WelcomeView
-function WelcomeView({ handleButtonClick }) {
-  let navigate = useNavigate();
-
-  const logout = async () => {
-      await signOut(auth); 
-      navigate("/");
-  };
-
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1 className='text'>Welcome to our website</h1>
-      <p className='text'>Please select your user type:</p>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '.5em' }}>
-  <button className='user-type' onClick={() => handleButtonClick('Patient')}>Patient</button>
-  <button className='user-type' onClick={() => handleButtonClick('Doctor')}>Doctor</button>
-  <button className='user-type' onClick={() => handleButtonClick('FDA')}>FDA</button>
-</div>
-      
-      <div>
-        <button onClick = {logout} className='back-btn'>Log Out</button>
-      </div>
-
-    </div>
-  );
-}
-
-// what is shown on PatientView
-function PatientView({ user, handleBackButtonClick }) {
-  return (
-    <div className='text'>
-      <h1>This is the patient view.</h1>
-      <h4> User logged in: </h4>
-      {user?.email}
-      <p className='text'>More patient text goes here</p>
-      <button className='back-btn' onClick={handleBackButtonClick}>Back</button>
-    </div>
-  );
-}
+};
 
 // what is shown on DoctorView
-function DoctorView({ user, handleBackButtonClick }) {
+function DoctorView({ user, LogOut }) {
   // can type in patient ID and it will display correct patient from Vendia
   const patientId = '0186b496-32f6-9a7f-cdfe-1e37ab416338';
   return (
@@ -127,20 +86,33 @@ function DoctorView({ user, handleBackButtonClick }) {
       </div>
       
       <p className='text'>More doctor text goes here</p>
-      <button className='back-btn' onClick={handleBackButtonClick}>Back</button>
+      <button className='back-btn' onClick={LogOut}>Sign Out</button>
     </div>
   );
 }
 
 // what is shown on FDAView. Line below h4 displays the user email logged in to FDA view
-function FDAView({ user, handleBackButtonClick }) {
+function FDAView({ user, LogOut }) {
   return (
     <div className='text'>
       <h1>This is the FDA view.</h1>
       <h4> User logged in: </h4>
       {user?.email}
       <p className='text'>More FDA text goes here</p>
-      <button className='back-btn' onClick={handleBackButtonClick}>Back</button>
+      <button className='back-btn' onClick={LogOut}>Sign Out</button>
+    </div>
+  );
+}
+
+// bavaria view (work in progress)
+function BavariaView({ user, LogOut }) {
+  return (
+    <div className='text'>
+      <h1>This is the Bavaria view.</h1>
+      <h4> User logged in: </h4>
+      {user?.email}
+      <p className='text'>More FDA text goes here</p>
+      <button className='back-btn' onClick={LogOut}>Sign Out</button>
     </div>
   );
 }
