e2f4aa5e91093ccc4d5e4ddf697d3d0910b9e0a6
Flakzaei <flakzaei@yahoo.com>
2023-03-29 23:09:58 -0700

    Updated the sign in page and FDA view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Nav = () => {
         <body>
         <div className = "background-color">
             <nav className="navbar">
-                <h1>Macrohard</h1>
+                <h1>MDMS</h1>
                 <div className="links">
                     <a href="/SignUp">Create Account</a>
                     <a href="/Login" style={{
