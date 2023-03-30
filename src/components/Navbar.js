9542db56f25e1ebcb81a0b9c18077ee47ed93445
Anthony Dominguez <anthonyd@Anthonys-MacBook-Air-3.local>
2023-03-29 20:51:12 -0700

    Homepage added-Looks like Figma

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,48 @@ const Nav = () => {
         <body>
         <div className = "background-color">
             <nav className="navbar">
-                <h1>Macrohard</h1>
+                <h1>MDMS</h1>
+                <h2>Medical Data</h2>
+                <h3>Management System</h3>
                 <div className="links">
                     <a href="/SignUp">Create Account</a>
                     <a href="/Login" style={{
-                        color: "white",
-                        backgroundColor: '#7f0a14',
-                        borderRadius: '10px'
+                        color: "#0E619C",
+                        backgroundColor: '#fff',
                     }}> Sign in </a>
                 </div>
             </nav>
             
         </div>
-        <div class="coder-name">
+        <div className="mission-head">
+            <h1> Our Mission </h1>
+        </div>
+        <div className="basic-info">
+            <p>
+            We have developed various programs, services, and initiatives in our hospitals that aim to enhance the health of the 
+            community and make healthcare more accessible. Through our collaborative partnerships, 
+            we strive to build connections to healthcare and community resources to promote better access and wellness.
+            </p>
+        </div>
+        
+        <div className="org-partners">
+            <h1>Partners</h1>
+        </div>
+
+        <div className="organizations">
+            {/* could probably change these from p tags to a href tags to make it interactable */}
+            <div className="background-hospital"></div>
+            <p className="hospital">Jane Hopkins Hospital</p>
+
+            <div className="background-fda"></div>
+            <p className="fda">U.S. Food and Drug Administration</p>
+
+            <div className="background-bavaria"></div>
+            <p className="bavaria">Bavaria Pharmaceutics</p>
+        </div>
+
+
+        {/* <div class="coder-name">
   <b> <i>Developers: </i></b><br></br>
         Angelo Karam (lead)<br></br>
         Rahul Gupta<br></br>
@@ -26,7 +55,7 @@ const Nav = () => {
         Farhad Lakzaei<br></br>
         Dakshina  Waduge<br></br>
         
-</div>
+</div> */}
         </body>
 
     );
