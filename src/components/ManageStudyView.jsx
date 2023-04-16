9bdb3e717428ba779c64a85fb7c0562b5e70b2f8
Angkaram <angelokaram@gmail.com>
2023-04-15 18:17:37 -0700

    I have more UI built on another local branch but i want to send the more stable build with a clickable notification button (just UI not functional) for now.

--- src/components/ManageStudyView.jsx
+++ src/components/ManageStudyView.jsx
@@ -4,35 +4,54 @@ import "./loginprompt.js";
 import './home.css';
 import { signOut } from "firebase/auth";
 import { auth } from "./firebase-config";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './DoctorView.css';
-import View from "./States_Views";
+import './Notifications.css';
 
-function ManageStudyView() { 
-  
+function ManageStudyView() {
   const location = useLocation();
-  const { user } = location.state;  //LogOut not working yet
+  const { user } = location.state;
   const navigate = useNavigate();
   const logout = async () => {
     await signOut(auth);
     navigate("/");
   };
 
-  console.log(user?.email);
-  return (
-<div className='managePatient'> 
-    <div className='doctorNavbar'>
+  const handleNotificationClick = () => {
+    const notificationCircle = document.querySelector('.notification-circle');
 
-      <div className='doctorViewTitle'>
-        <div className='janeHopkinsTitleText'>Jane Hopkins</div>
-        <div className='hospitalTitleText'>Hospital</div>
+    notificationCircle.classList.toggle('clicked');
+  };
+
+  const handlePopupClick = () => {
+    const notificationCircle = document.querySelector('.notification-circle');
+
+    notificationCircle.classList.remove('clicked');
+  };
+
+  return (
+    <div className='managePatient'> 
+      <div className='doctorNavbar'>
+        <div className='doctorViewTitle'>
+          <div className='janeHopkinsTitleText'>Jane Hopkins</div>
+          <div className='hospitalTitleText'>Hospital</div>
+        </div>
+        <div className='displayEmail'>{user?.email}</div>
+        <button className='signOutButton' onClick={logout}>
+          <div className='signOutIcon'></div>
+          <div className='signOutText'>Sign Out</div>
+        </button>
       </div>
-      <div className='displayEmail'>{user?.email}</div>
-      <button className='signOutButton' onClick={logout}>
-        <div className='signOutIcon'></div>
-        <div className='signOutText'>Sign Out</div>
-      </button>
+      <div class="notification-circle" onClick={handleNotificationClick}>
+        <div class="notification-circle-icon"></div>
+        <div class="notification-number">1</div>
+      </div>
+      <div class="notification-popup" onClick={handlePopupClick}>
+        <p>The study was approved</p>
+      </div>
+      
     </div>
-</div>)}
+  );
+}
 
 export default ManageStudyView;
