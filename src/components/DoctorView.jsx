1480604a6fece4b26c9ca0378f203fb9ed33578d
Anthony Dominguez <anthonyd@Anthonys-MacBook-Air-3.local>
2023-04-29 15:00:14 -0700

    Scaled Manage Patient View for Jane Hopkins

--- src/components/DoctorView.jsx
+++ src/components/DoctorView.jsx
@@ -11,8 +11,8 @@ import DisplayPatientData from './DisplayPatientData';
 import './DoctorView.css';
 import View from "./States_Views";
 
-function DoctorView() { 
-  
+function DoctorView() {
+
   const location = useLocation();
   const { user } = location.state;  //LogOut not working yet
   const navigate = useNavigate();
@@ -20,7 +20,7 @@ function DoctorView() {
     await signOut(auth);
     navigate("/");
   };
-  
+
   const [nameSearch, setNameSearch] = useState("");
   const [ageSearch, setAgeSearch] = useState("");
   const [insuranceSearch, setInsuranceSearch] = useState("");
@@ -40,76 +40,87 @@ function DoctorView() {
     navigate("/Appointments", { state: { user } });
   };
 
+  const DoctorHomePage = () => {
+    navigate("/View", { state: { user } });
+  };
+
   // can type in patient ID and it will display correct patient from Vendia
   const patientId = '0186b496-32f6-9a7f-cdfe-1e37ab416338';
   console.log(user?.email);
   return (
-    <div className='managePatient'> 
-
-    <div className='doctorNavbar'>
-
-      <div className='doctorViewTitle'>
+    <div className='managePatient'>
+      {/* NTS: REVERT BACK IF FAILS + DoctorView.css */}
+      <div className = 'nav-bar'>
         <div className='janeHopkinsTitleText'>Jane Hopkins
           <div className='hospitalTitleText'>Hospital</div>
         </div>
-      </div>
-      <div className='displayEmail'>{user?.email}</div>
-      <button className='signOutButton' onClick={logout}>
-        <div className='signOutIcon'></div>
-        <div className='signOutText'>Sign Out</div>
-      </button>
-    </div>
-    
-    <div className='doctorNavButtonLocations'>
-      <div>
-      <Link to="/View">
-        <button className='welcomeContainer'>
-          <div className='welcomeText'>Welcome Page</div>
-        </button>
-      </Link>
-      </div>
-      <div className='appointmentContainer'>
-        <button onClick={() => DoctorAppointments(user)}> Manage Appointments</button>
-      </div>
-      <div>
-        <button onClick={togglePopup} className='addPatientContainer'>
-          <div className='addPatientText'>Add Patients</div>
+        <div className='displayEmail'>{user?.email}</div>
+        <button className='signOutButton' onClick={logout}>
+          <div className='signOutIcon'></div>
+          <div className='signOutText'>Sign Out</div>
         </button>
       </div>
+
+      <div className='doctorNavButtonLocations'>
+        <div className="welcomeBro">
+          <button onClick={() => DoctorHomePage(user)}>Welcome Page</button>
+        </div>
+
+        {/* <div className='welcomeBro'>
+          <button>Welcome Page</button>
+        </div> */}
+
+        <div className='appointmentBro'>
+          <button onClick={() => DoctorAppointments(user)}>Manage Appointments</button>
+        </div>
+
+        <div className='addPatientBro'>
+          <button onClick={togglePopup} >Add Patients</button>
+        </div>
+        
+      </div>
+
+      <div className='patientSearchBox'>
+        <div className='patientSearchBoxName'>Patient Search</div>
+
+        <div className='patientSearchBars'>
+
+          <div className='patientNameSearch'>
+            <div className='patientNameSearchLabel'>Name</div>
+            <input className='patientNameSearchBox' type="text" onChange={(event) => { setNameSearch(event.target.value) }} value={nameSearch} />
+          </div>
+
+          <div className = 'patientAgeSearch'>
+            <div className='patientAgeSearchLabel'>Age</div>
+            <input className='patientAgeSearchBox' type="number" onChange={(event) => { setAgeSearch(event.target.value) }} value={ageSearch} />
+          </div>
+
+          <div className = 'patientICDSearch'>
+            <div className='patientICDSearchLabel'>ICD Healthcode</div>
+            <input className='patientICDSearchBox' type="text" onChange={(event) => { setICDSearch(event.target.value) }} value={ICDSearch} />
+          </div>
+
+          <div className = 'patientInsuranceSearch'>
+            <div className='patientInsuranceSearchLabel'>Insurance Number</div>
+            <input className='patientInsuranceSearchBox' type="text" onChange={(event) => { setInsuranceSearch(event.target.value) }} value={insuranceSearch} />
+          </div>
+
+        </div>
+
+        <div className='searchUndoLocations'>
+
+          <button onClick={clearSearch} className='searchButton'> Clear Search </button>
+
+        </div>
+      </div>
+
+      <div className='patientTableLocation'>
+        <DisplayPatientData nameSearch={nameSearch} insuranceSearch={insuranceSearch} ICDSearch={ICDSearch} patientId={patientId} />
+      </div>
+
+      {isOpen && <AddPatientButton handleClose={togglePopup} />}
+
     </div>
-
-    <div className='patientSearchBox'>
-      <div className='patientSearchBoxName'>Patient Search</div>
-      <div className='searchUndoLocations'>
-
-      <button onClick = {clearSearch} className='searchButton'> Clear Search </button>
-
-      </div>
-      <div className='patientNameSearch'>      
-          <input className='patientNameSearchBox' type="text" onChange = {(event) => {setNameSearch(event.target.value)}} value={nameSearch}/>
-          <div className='patientNameSearchLabel'>Name</div>
-      </div>
-      <div className='patientAgeSearch'>
-          <input className='patientAgeSearchBox' type="number" onChange = {(event) => {setAgeSearch(event.target.value)}} value={ageSearch}/>
-          <div className='patientAgeSearchLabel'>Age</div>
-      </div>
-      <div className='patientICDSearch'>
-        <input className='patientICDSearchBox' type="text" onChange = {(event) => {setICDSearch(event.target.value)}} value={ICDSearch}/>
-        <div className='patientICDSearchLabel'>ICD Healthcode</div>
-      </div>
-      <div className='patientInsuranceSearch' >
-          <input className='patientInsuranceSearchBox' type="text" onChange = {(event) => {setInsuranceSearch(event.target.value)}} value={insuranceSearch}/>
-          <div className='patientInsuranceSearchLabel'>Insurance Number</div>
-      </div>
-    </div>
-
-    <div className='patientTableLocation'>
-      <DisplayPatientData nameSearch={nameSearch} insuranceSearch={insuranceSearch} ICDSearch={ICDSearch} patientId={patientId}/>
-    </div>
-
-    {isOpen && <AddPatientButton handleClose={togglePopup}/>}
-
-  </div>
   );
 }
 
