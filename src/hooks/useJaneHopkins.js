99240410d1f0af81274a09f218b827916d293ddc
rahulio96 <rahgupta96@gmail.com>
2023-05-09 00:42:14 -0700

    Change hooks/vendia folder to work with new uni

--- src/hooks/useJaneHopkins.js
+++ src/hooks/useJaneHopkins.js
@@ -1,9 +1,9 @@
 import { createVendiaClient } from "@vendia/client";
 
 const client = createVendiaClient({
-    apiUrl: `https://waw9fm8vki.execute-api.us-west-2.amazonaws.com/graphql/`,
-    websocketUrl: `wss://xedwlaswyh.execute-api.us-west-2.amazonaws.com/graphql`,
-    apiKey: `9YaJcrJzJ5srWmcosaNENLW9td6WJhniPP6WZPUdvLCi`, // <---- API key
+    apiUrl: `https://r3yygay3z2.execute-api.us-west-2.amazonaws.com/graphql/`,
+    websocketUrl: `wss://k2wuqyhp0d.execute-api.us-west-2.amazonaws.com/graphql`,
+    apiKey: `AXKe8Rj4xQ9uPetWnA3Mi2yyrDLB9tc4ibS9YjNbyEwS`, // <---- API key
 })
 
 const {entities} = client;
