4aca577920ec8ec0e7aaaf88a3d07afd52e0d91f
rahulio96 <rahgupta96@gmail.com>
2023-05-23 19:09:27 -0700

    Fixing 404 Error

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { HashRouter as Router } from 'react-router-dom';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
